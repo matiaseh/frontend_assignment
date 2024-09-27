7157cce730af9c135bae591f81f4d22373982e35
Matias Hynnä <hynna.matias@gmail.com>
2024-09-27 13:50:34 +0300

    Minor app and root style changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,21 @@ import {
 } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import Navigation from './components/Navigation/Navigation';
-import './index.css';
 
-const AppRoutes: React.FC = () => {
+const App: React.FC = () => {
   return (
-    <Routes>
-      {/* Add some home page content */}
-      <Route path='' element={<div>Home</div>} />
-      <Route path='/dashboard' element={<Dashboard />} />
-
-      {/* Catch all unknown routes and redirect */}
-      <Route path='*' element={<Navigate to='/' />} />
-    </Routes>
-  );
-};
-
-const App: React.FC = () => (
-  <div className='app-container'>
     <Router>
       <Navigation />
-      <AppRoutes />
+      <Routes>
+        {/* Add some home page content */}
+        <Route path='' element={<div>Home</div>} />
+        <Route path='/dashboard' element={<Dashboard />} />
+
+        {/* Catch all unknown routes and redirect */}
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
     </Router>
-  </div>
-);
+  );
+};
 
 export default App;
