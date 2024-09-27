2fd382be1fb8a773770eb8ff5730d4811e0d9c74
Matias Hynnä <hynna.matias@gmail.com>
2024-09-27 13:02:38 +0300

    Add router and basic route to show dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
 import './index.css';
 
 const App: React.FC = () => {
   return (
     <div className='app-container'>
-      <Dashboard />
+      <Router>
+        <Routes>
+          <Route path='' element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
