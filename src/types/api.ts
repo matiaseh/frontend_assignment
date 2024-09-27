05553f29e3ccddb22a64120ab549957a885d148e
Matias Hynnä <hynna.matias@gmail.com>
2024-09-27 11:39:42 +0300

    Add types for the Users

---
+++ src/types/api.ts
@@ -0,0 +1,25 @@
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: {
+    lat: string;
+    lng: string;
+  };
+}
+
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: string;
+  website: string;
+  address: Address;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
